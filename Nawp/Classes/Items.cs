d6ea5ce1fb551794d39b40c30a4c88c05cf8df6d
Ang3lERR404 <yukohelpme565@gmail.com>
2025-08-13 10:23:13 -0700

    Add files via upload

---
+++ Nawp/Classes/Items.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BepInEx;
+using Data;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.Tilemaps;
+
+#nullable disable
+namespace Rend.Classes;
+
+[CreateAssetMenu(fileName = "Items", menuName = "Rend/Items", order = 1)]
+public class Items : ScriptableObject {
+  public
+    string name;
+    string desc;
+    EntityData e;
+    int id;
+    Material mat;
+    VisualEffectAsset vfxa;
+    Gradient fxgrad;
+    Gradient collGrad;
+    bool isStruct;
+    bool craftable;
+    #if isStruct == true
+      StructureData sdt;
+      bool crafter;
+      #if crafter == true
+      #endif
+    #endif
+
+    [Serializable]
+    public class DensitySpriteList {
+      public
+        int thrshDens;
+        TileBase tileRule;
+    }
+}
