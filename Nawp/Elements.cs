3d002213f10f2c1f1adf53ef936b2b4e8dd489b8
Ang3lERR404 <yukohelpme565@gmail.com>
2025-08-12 23:45:50 -0700

    Add files via upload

---
+++ Nawp/Elements.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace Rend;
+/// <summary>
+/// This is more or less the Element Manager
+/// allowing for easy adding, management, or even deletion.
+/// It's made as a Dictionary to more easily extend upon it
+/// Since the process of making an enum at runtime- forsomefuckingreason
+/// is complicated.
+/// </summary>
+public class Elements {
+  public
+    Dictionary<string, int> elems = new();
+
+  public Dictionary<string, int> constructor() {
+    this.elems.Add("Life", 0);
+    this.elems.Add("Earth", 1);
+    this.elems.Add("Fire", 2);
+    this.elems.Add("Shadow", 3);
+    this.elems.Add("Air", 4);
+    this.elems.Add("Water", 5);
+    this.elems.Add("Salt", 6);
+    return this.elems;
+  }
+
+  /// <summary>
+  /// Adds a new element to the dictionary.
+  /// </summary>
+  /// <param name="name">The name of the element</param>
+  /// <param name="v">The integer value of the element</param>
+  public void Add(string name, int v) {
+    this.elems.Add(name, v);
+  }
+
+  /// <summary>
+  /// Checks if the given element is in the dictionary.
+  /// </summary>
+  /// <param name="name">The name of the element</param>
+  /// <returns></returns>
+  public bool has(string name) {
+    return this.elems.ContainsKey(name);
+  }
+
+  /// <summary>
+  /// Removes an element from the dictionary.
+  /// </summary>
+  /// <param name="name">The name of the element</param>
+  public void rem(string name) {
+    this.elems.Remove(name);
+  }
+
+  /// <summary>
+  /// Planned, hides an element from the ui.
+  /// </summary>
+  /// <param name="name">The name of the element</param>
+  public void hide(string name) {
+    //noop
+  }
+}
---
+++ Nawp/Rend.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+// ReSharper disable InvertIf
+
+#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+namespace Rend;
+
+[BepInPlugin(GUID, Name, VERSION)]
+public class Rend {
+  const string GUID = "ang3l404";
+  const string Name = "Rend";
+  const string VERSION = "1.0.0";
+  internal static ManualLogSource Log;
+  readonly Harmony h = new(GUID);
+
+  void Awake() {
+    Log = Logger;
+    Log.LogInfo($"Plugin {Name} loaded");
+    h.PatchAll();
+  }
+
+  // [HarmonyPatch(typeof(Upgrades), "Awake")]
+  // class UpgradesAwakePatch {
+  //   [HarmonyPostfix]
+  //   public static void Postfix(Upgrades inst) {
+  //     Log.LogInfo("Upgrades.Awake() Completed, Running postfix.");
+  //     if (inst == null) {
+  //       Log.LogError("The instance was null");
+  //       return;
+  //     }
+  //   }
+  // }
+  
+}
